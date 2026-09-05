f56e232c3368fd0e2e27d67949a87f70fb39034f
fersonaglio <52318331+fersonaglio@users.noreply.github.com>
2026-09-05 16:59:55 +0900

    feat(itinerary): add Gui's Burger (@meetyourwagyu) 7th best burger in the world to Day 10 lunch

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Service Worker for Tabi Japan 2026 PWA
-const CACHE_NAME = 'tabi-japan-v16-cache';
+const CACHE_NAME = 'tabi-japan-v17-cache';
 const ASSETS_TO_CACHE = [
   './',
   './index.html',
