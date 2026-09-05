4bbcb48ab4ffe2c059d0a82a19206c407d5f0d1d
fersonaglio <52318331+fersonaglio@users.noreply.github.com>
2026-09-06 08:15:43 +0900

    feat(day6): update itinerary with sneakers, JINS glasses, Tokyo National Museum (samurais), The Railway Museum and Shinkansen to Kyoto

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Service Worker for Tabi Japan 2026 PWA
-const CACHE_NAME = 'tabi-japan-v26-cache';
+const CACHE_NAME = 'tabi-japan-v27-cache';
 const ASSETS_TO_CACHE = [
   './',
   './index.html',
