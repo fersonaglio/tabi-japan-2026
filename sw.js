7aff85715c8b56cf396ae612649e98a88bd0c93a
fersonaglio <52318331+fersonaglio@users.noreply.github.com>
2026-09-07 00:04:07 +0900

    fix(html): remover sequencias literais de \n\n entre os cards dos dias

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Service Worker for Tabi Japan 2026 PWA
-const CACHE_NAME = 'tabi-japan-v32-cache';
+const CACHE_NAME = 'tabi-japan-v33-cache';
 const ASSETS_TO_CACHE = [
   './',
   './index.html',
