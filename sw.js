9cb3b40539351017b2d8d62bb152306d9d9b6431
fersonaglio <52318331+fersonaglio@users.noreply.github.com>
2026-09-07 13:31:53 +0900

    feat(maps): adicionar links hiperprecisos do Google Maps nos cards de Quioto (Dia 07 e Dia 08)

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Service Worker for Tabi Japan 2026 PWA
-const CACHE_NAME = 'tabi-japan-v34-cache';
+const CACHE_NAME = 'tabi-japan-v36-cache';
 const ASSETS_TO_CACHE = [
   './',
   './index.html',
